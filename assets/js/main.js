c9351af194316d91c688667b3db2b147eb62def0
Edd <smith.edward@hotmail.com>
2018-02-22 21:13:17 +0000

    transition effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,23 @@
 $(document).ready(function(){
+
+	var shiftValues = [0.20, 0.12, 0.01, -0.24, -0.34];
+
 	var supaScroll = false;
 	var startingX;
 	var currentIndex = 1;
+
+	get_max_shift();
+
+	function get_max_shift(){
+		$('.background__segment').each(function(){
+			var slide_index = $(this).parent().parent().index();
+			var max_shift = ( $(window).width() * shiftValues[$(this).index()] ) * slide_index;
+			
+			$(this).find('div').attr('data-max-shift', max_shift).css('left', max_shift + 'px');
+
+			$(this).parent().attr('data-slide-offset', ( ( $(window).width() * 0.2 ) * slide_index )).css('transform','translateX(-' + ( ( $(window).width() * 0.2 ) * slide_index ) + 'px)')
+		})
+	}
 
 	$(document).on('mousedown touchstart', '.can-transition', function(e){
 
@@ -17,9 +33,9 @@ $(document).ready(function(){
 		e.preventDefault();
 		
 		if ( supaScroll ) {
+			background_shift(event.clientX - startingX);
 
 			$(this).css('transform','translateX(' + ( ( event.clientX - startingX ) + ( ( $(window).width() * ( currentIndex - 1 ) ) * -1 ) ) + 'px)');	
-
 		}
 
 	})
@@ -29,43 +45,26 @@ $(document).ready(function(){
 		e.preventDefault();
 
 		if ( ( ( event.clientX - startingX ) * -1 ) > ( $(window).width() / 10 ) ) {
-
-			$(this).removeClass('can-transition').addClass('transitioning');
+			slider_classes($(this));
 
 			if ( currentIndex < $('.hero__slide').length ) {
-				
-				$('.hero').css('transform','translateX(-' + ( $(window).width() * currentIndex ) + 'px)');
-
+				normalise_slider(0);
 				currentIndex++;
-
 			} else {
-
-				normalise_slider();
-
+				normalise_slider(1);
 			}
-
 		} else if ( ( ( event.clientX - startingX ) * -1 ) < ( ( $(window).width() / 10 ) * -1 ) ) {
-
-			$(this).removeClass('can-transition').addClass('transitioning');
+			slider_classes($(this));
 
 			if ( currentIndex > 1 ) {
-				
-				$('.hero').css('transform','translateX(-' + ( $(window).width() * ( currentIndex - 2) ) + 'px)');
-
+				normalise_slider(2);
 				currentIndex--;
-
 			} else {
-
-				normalise_slider();
-
+				normalise_slider(1);
 			}
-			
 		} else {
-
-			$(this).removeClass('can-transition').addClass('transitioning');
-
-			normalise_slider();
-
+			slider_classes($(this));
+			normalise_slider(1);
 		}
 
 		supaScroll = false;
@@ -76,8 +75,53 @@ $(document).ready(function(){
 
 	})
 
-	function normalise_slider(){
-		$('.hero').css('transform','translateX(-' + ( $(window).width() * ( currentIndex - 1) ) + 'px)');
+	function normalise_slider(e){
+		$('.hero').css('transform','translateX(-' + ( $(window).width() * ( currentIndex - e) ) + 'px)');
+
+		$('.background__segment').each(function(){
+			var slide_index = $(this).parent().parent().index();
+			var multiplier = slide_index - currentIndex;
+			var index = $(this).index();
+
+			$(this).find('div').addClass('transitioning').css('left', ( $(this).find('div').attr('data-max-shift') * multiplier ) + 'px' );
+
+			setTimeout(function(){
+				$('.background__segment > div').each(function(){ $(this).removeClass('transitioning'); })
+			}, 600)
+		})
+
+		$('.background').each(function(){
+			if ( $(this).parent().index() != 0 ) {
+				$(this).addClass('transitioning').css('transform','translateX(-' + ( $(this).attr('data-slide-offset') - ( ( $(window).width() * 0.2 ) * currentIndex ) ) + 'px)');
+			}
+
+			setTimeout(function(){
+				$('.background').each(function(){ $(this).removeClass('transitioning'); })
+			}, 600)
+		})
+	}
+
+	function slider_classes(e){
+		e.removeClass('can-transition').addClass('transitioning');
+	}
+
+	function background_shift(diffX){
+		$('.background').each(function(){
+			if ( $(this).parent().index() != 0 ) {
+				$(this).css('transform','translateX(-' + ( $(this).attr('data-slide-offset') - ( ( diffX * 0.08) * -1 ) ) + 'px)');
+			}
+		})
+
+		$('.background__segment').each(function(){
+			var index = $(this).index();
+			var current_shift_val = $(this).find('div').attr('data-max-shift');
+
+			var new_offset = parseInt(current_shift_val) + ( diffX * shiftValues[index] );
+			console.log(new_offset);
+
+			$(this).find('div').css('left','' + new_offset + 'px');
+		})
+
 	}
 
 })
